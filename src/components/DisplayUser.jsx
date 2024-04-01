1fb7671aab8711c210f741c3a6ac8c2acbb3b798
GGTuran <tayyabturan67@gmail.com>
2024-04-01 12:18:08 +0600

    deleting a user from database

--- src/components/DisplayUser.jsx
+++ src/components/DisplayUser.jsx
@@ -10,10 +10,34 @@ const DisplayUser = () => {
     const users = useLoaderData();
     console.log(users);
 
+    const handleDelete = _id =>{
+        console.log(_id);
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method:"DELETE",
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.deletedCount > 0){
+                alert("User Deleted successfully");
+            }
+        })
+    }
 
     return (
         <div>
         <h1>User</h1>
+        {
+            users.map(user => (
+                <div key={user._id}>
+                    <h1>{user.name}</h1>
+                    <span>{user.email}</span>
+                    <br />
+                    <button onClick={() => handleDelete(user._id)} type="submit">Delete</button>
+
+                </div>
+            ))
+        }
         </div>
     );
 };
